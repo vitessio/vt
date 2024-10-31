22250bcfc1fc469261cf8f4ded8d3110c2f31abf
Florent Poinsard <florent.poinsard@outlook.fr>
2024-10-31 17:48:49 -0600

    Add join predicates to benchstat output
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/benchstat/benchstat.go
+++ go/benchstat/benchstat.go
@@ -301,19 +301,11 @@ func printKeysSummary(out io.Writer, file readingSummary) {
 	_, _ = fmt.Fprintf(out, "Summary from trace file %s\n", file.Name)
 	tableSummaries, failuresSummaries := summarizeQueries(file.AnalysedQueries)
 	for _, summary := range tableSummaries {
-		table := tablewriter.NewWriter(out)
-		table.SetAutoFormatHeaders(false)
-		table.SetHeader([]string{"Column", "Filter %", "Grouping %", "Join %"})
-		for colName, usage := range summary.GetColumns() {
-			table.Append([]string{
-				colName,
-				fmt.Sprintf("%.2f%%", usage.FilterPercentage),
-				fmt.Sprintf("%.2f%%", usage.GroupingPercentage),
-				fmt.Sprintf("%.2f%%", usage.JoinPercentage),
-			})
-		}
 		fmt.Fprintf(out, "Table: %s used in %d queries\n", summary.Table, summary.QueryCount)
-		table.Render()
+
+		renderColumnUsageTable(out, summary)
+		renderJoinPredicatesTable(out, summary)
+
 		_, _ = fmt.Fprintln(out)
 	}
 
@@ -330,6 +322,35 @@ func printKeysSummary(out io.Writer, file readingSummary) {
 	}
 }
 
+func renderColumnUsageTable(out io.Writer, summary TableSummary) {
+	table := createTableWriter(out, []string{"Column", "Filter %", "Grouping %", "Join %"})
+	for colName, usage := range summary.GetColumns() {
+		table.Append([]string{
+			colName,
+			fmt.Sprintf("%.2f%%", usage.FilterPercentage),
+			fmt.Sprintf("%.2f%%", usage.GroupingPercentage),
+			fmt.Sprintf("%.2f%%", usage.JoinPercentage),
+		})
+	}
+	table.Render()
+}
+
+func renderJoinPredicatesTable(out io.Writer, summary TableSummary) {
+	table := createTableWriter(out, []string{"Join Predicate"})
+	for _, predicate := range summary.JoinPredicates {
+		table.Append([]string{predicate.String()})
+	}
+	table.Render()
+}
+
+func createTableWriter(out io.Writer, cols []string) *tablewriter.Table {
+	table := tablewriter.NewWriter(out)
+	table.SetAutoFormatHeaders(false)
+	table.SetHeader(cols)
+	table.SetAutoWrapText(false)
+	return table
+}
+
 type ColumnUsage struct {
 	FilterPercentage   float64
 	GroupingPercentage float64
@@ -337,10 +358,11 @@ type ColumnUsage struct {
 }
 
 type TableSummary struct {
-	Table      string
-	QueryCount int
-	Columns    map[string]ColumnUsage
-	Failed     bool
+	Table          string
+	QueryCount     int
+	Columns        map[string]ColumnUsage
+	JoinPredicates []operators.JoinPredicate
+	Failed         bool
 }
 
 type FailuresSummary struct {
@@ -380,34 +402,8 @@ func summarizeQueries(queries *keys.Output) ([]TableSummary, []FailuresSummary) 
 			}
 			tableUsageCounts[table] += query.UsageCount
 
-			updateColumnUsage := func(columns any, usageType func(*ColumnUsage) *float64) {
-				var colNames []string
-				switch columns := columns.(type) {
-				case []operators.Column:
-					slice.Map(columns, func(col operators.Column) interface{} {
-						colNames = append(colNames, col.String())
-						return col
-					})
-				case []operators.ColumnUse:
-					slice.Map(columns, func(col operators.ColumnUse) interface{} {
-						colNames = append(colNames, col.Column.String())
-						return col
-					})
-				}
-
-				for _, col := range colNames {
-					if strings.HasPrefix(col, table+".") {
-						colName := strings.TrimPrefix(col, table+".")
-						usage := tableSummaries[table].Columns[colName]
-						*usageType(&usage) += float64(query.UsageCount)
-						tableSummaries[table].Columns[colName] = usage
-					}
-				}
-			}
-
-			updateColumnUsage(query.FilterColumns, func(cu *ColumnUsage) *float64 { return &cu.FilterPercentage })
-			updateColumnUsage(query.GroupingColumns, func(cu *ColumnUsage) *float64 { return &cu.GroupingPercentage })
-			updateColumnUsage(query.JoinColumns, func(cu *ColumnUsage) *float64 { return &cu.JoinPercentage })
+			summarizeColumnUsage(table, tableSummaries, query)
+			summarizeJoinPredicates(query.JoinPredicates, table, tableSummaries)
 		}
 	}
 
@@ -444,4 +440,51 @@ func summarizeQueries(queries *keys.Output) ([]TableSummary, []FailuresSummary) 
 	}
 
 	return result, failures
+}
+
+func summarizeColumnUsage(table string, tableSummaries map[string]*TableSummary, query keys.QueryAnalysisResult) {
+	updateColumnUsage := func(columns any, usageType func(*ColumnUsage) *float64) {
+		var colNames []string
+		switch columns := columns.(type) {
+		case []operators.Column:
+			slice.Map(columns, func(col operators.Column) interface{} {
+				colNames = append(colNames, col.String())
+				return col
+			})
+		case []operators.ColumnUse:
+			slice.Map(columns, func(col operators.ColumnUse) interface{} {
+				colNames = append(colNames, col.Column.String())
+				return col
+			})
+		}
+
+		for _, col := range colNames {
+			if strings.HasPrefix(col, table+".") {
+				colName := strings.TrimPrefix(col, table+".")
+				usage := tableSummaries[table].Columns[colName]
+				*usageType(&usage) += float64(query.UsageCount)
+				tableSummaries[table].Columns[colName] = usage
+			}
+		}
+	}
+
+	updateColumnUsage(query.FilterColumns, func(cu *ColumnUsage) *float64 { return &cu.FilterPercentage })
+	updateColumnUsage(query.GroupingColumns, func(cu *ColumnUsage) *float64 { return &cu.GroupingPercentage })
+	updateColumnUsage(query.JoinColumns, func(cu *ColumnUsage) *float64 { return &cu.JoinPercentage })
+}
+
+func summarizeJoinPredicates(joinPredicates []operators.JoinPredicate, table string, tableSummaries map[string]*TableSummary) {
+outer:
+	for _, predicate := range joinPredicates {
+		if predicate.LHS.Table != table && predicate.RHS.Table != table {
+			// should never be true, but just in case something went wrong
+			continue
+		}
+		for _, joinPredicate := range tableSummaries[table].JoinPredicates {
+			if joinPredicate.Equal(predicate) {
+				continue outer
+			}
+		}
+		tableSummaries[table].JoinPredicates = append(tableSummaries[table].JoinPredicates, predicate)
+	}
 }
